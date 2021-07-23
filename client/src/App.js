6babf928a4d3968a242fcb1af689b9685e502b24
Cleisson <cleissonconstc@gmail.com>
2021-07-23 17:28:23 -0300

    add fontawesome and bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+   return (
+      <div className="container mt-4">
+         <h4 className="display-4 text-center mb-4">
+            <i className="fab fa-react"> </i> <i class="fab fa-js-square"></i> File Uploader
+         </h4>
+      </div>
+   );
+}
+
+export default App;
