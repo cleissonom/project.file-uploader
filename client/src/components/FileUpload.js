c469f08b1fa1ff8fd099d3e4c61edb53a4b69de6
Cleisson <cleissonconstc@gmail.com>
2021-07-27 14:34:25 -0300

    adding disabled button when file is empty

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,18 +1,27 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 export const FileUpload = () => {
+   const [file, setFile] = useState('')
+   const [fileName, setFileName] = useState('Choose File')
+
+   const onChange = e => {
+      setFile(e.target.files[0]);
+      setFileName(e.target.files[0].name);
+   }
+
    return (
       <Fragment>
          <form>
             <div className="custom-file">
-               <input type="file" className="custom-file-input w-100" id="customFile" />
-               <label className="custom-file-label" for="customFile">Choose file</label>
+               <input type="file" className="custom-file-input w-100" id="customFile" onChange={onChange} />
+               <label className="custom-file-label" for="customFile">{fileName}</label>
             </div>
 
             <input
                type="submit"
                value="Upload"
                className="btn btn-primary btn-block mt-4"
+               disabled={fileName === 'Choose File' ? true : false}
             />
          </form>
       </Fragment>
