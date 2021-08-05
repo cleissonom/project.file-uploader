c6de3e2bfe3f1d44c78300bb5ea4530104709b35
Cleisson <cleissonconstc@gmail.com>
2021-08-05 11:16:41 -0300

    finishing the project with an unresolved error

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,15 +1,19 @@
-import React, { Fragment, useState } from 'react'
-import axios from 'axios'
+import React, { Fragment, useState } from 'react';
+import Message from './Message';
+import Progress from './Progress';
+import axios from 'axios';
 
 export const FileUpload = () => {
-   const [file, setFile] = useState('')
-   const [fileName, setFileName] = useState('Choose File')
-   const [uploadedFile, setUploadedFile] = useState({})
+   const [file, setFile] = useState('');
+   const [filename, setFilename] = useState('Choose File');
+   const [uploadedFile, setUploadedFile] = useState({});
+   const [message, setMessage] = useState('');
+   const [uploadPercentage, setUploadPercentage] = useState(0);
 
    const onChange = e => {
       setFile(e.target.files[0]);
-      setFileName(e.target.files[0].name);
-   }
+      setFilename(e.target.files[0].name);
+   };
 
    const onSubmit = async e => {
       e.preventDefault();
@@ -20,45 +24,66 @@ export const FileUpload = () => {
          const res = await axios.post('/upload', formData, {
             headers: {
                'Content-Type': 'multipart/form-data'
+            },
+            onUploadProgress: progressEvent => {
+               setUploadPercentage(
+                  parseInt(
+                     Math.round((progressEvent.loaded * 100) / progressEvent.total)
+                  )
+               );
             }
          });
 
+         // Clear percentage
+         setTimeout(() => setUploadPercentage(0), 10000);
+
          const { fileName, filePath } = res.data;
 
-         setUploadedFile({ fileName, filePath })
+         setUploadedFile({ fileName, filePath });
+
+         setMessage('File Uploaded');
       } catch (err) {
          if (err.response.status === 500) {
-            console.log('There was a problem with the server')
+            setMessage('There was a problem with the server');
          } else {
-            console.log(err.response.data.msg)
+            setMessage(err.response.data.msg);
          }
+         setUploadPercentage(0)
       }
-   }
+   };
 
    return (
       <Fragment>
+         {message ? <Message msg={message} /> : null}
          <form onSubmit={onSubmit}>
-            <div className="custom-file">
-               <input type="file" className="custom-file-input w-100" id="customFile" onChange={onChange} />
-               <label className="custom-file-label" htmlFor="customFile">{fileName}</label>
+            <div className='custom-file mb-4'>
+               <input
+                  type='file'
+                  className='custom-file-input'
+                  id='customFile'
+                  onChange={onChange}
+               />
+               <label className='custom-file-label' htmlFor='customFile'>
+                  {filename}
+               </label>
             </div>
 
+            <Progress percentage={uploadPercentage} />
+
             <input
-               type="submit"
-               value="Upload"
-               className="btn btn-primary btn-block mt-4"
-               disabled={fileName === 'Choose File' ? true : false}
+               type='submit'
+               value='Upload'
+               className='btn btn-primary btn-block mt-4'
             />
          </form>
          {uploadedFile ? (
-            <div className="row mt-5">
-               <div className="col-md-6 m-auto">
-                  <h3 className="text-center">{uploadedFile.fileName}</h3>
+            <div className='row mt-5'>
+               <div className='col-md-6 m-auto'>
+                  <h3 className='text-center'>{uploadedFile.fileName}</h3>
                   <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
                </div>
             </div>
          ) : null}
       </Fragment>
-   )
-}
-
+   );
+};
